2fdb458e0b2195de3cc35a51e6c0d13fa04f3193
AHNDOORI <kaudo@msn.com>
2023-08-18 14:09:19 +0900

    TEST

--- conceptvideo/deploy.sh
+++
@@ -1,9 +0,0 @@
-sudo mkdir -p /server/log/relay_model
-sudo chown -R brsweb:brsweb /server/log/relay_model
-sudo rm -rf /server/relay_model
-sudo mv /server/relay_model_build /server/relay_model
-sudo chown -R brsweb:brsweb /server/relay_model
-cd /server/relay_model
-pm2 delete "RELAY-MODEL"
-pm2 start /server/relay_model/relay_model_prd.json
-pm2 save --force
